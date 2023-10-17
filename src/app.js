bf79ec7520e6438a236c823a5d10ca5718f87511
Fernando R. E. Castillo R. <fer.castillorubilar@gmail.com>
2023-10-17 17:23:26 -0300

    push

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  var cards = ["♥", "♦ ", "♣", "♠"];
+  var numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+  function Generador() {
+    var pickCard = Math.floor(Math.random() * cards.length);
+    var pickNumber = Math.floor(Math.random() * numbers.length);
+  var showCard = `${cards[pickCard]}`;
+  var showNumber = `${numbers[pickNumber]}`;
+
+    document.getElementById('top-corner').innerHTML = showCard;
+    document.getElementById('card-number').innerHTML = showNumber;
+    document.getElementById('bottom-corner').innerHTML = showCard
+  }
+  console.log(Generador());
+}
